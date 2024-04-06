8a3fa7104439911694f67c03e774d0e14afe843f
Victor Cavalcanti <victorcmarques.dev@gmail.com>
2024-04-06 09:16:56 -0300

    Terceiro Commit

--- app/src/main/java/com/example/app23/MainActivity.kt
+++ app/src/main/java/com/example/app23/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val i = 10
+        val i = 20
     }
 }
