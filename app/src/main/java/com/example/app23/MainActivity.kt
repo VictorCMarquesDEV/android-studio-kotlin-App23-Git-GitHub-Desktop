e9d8741372162e6480837bd14a1450a8774a76fe
VictorCMarquesDEV <victorcmarques.dev@gmail.com>
2024-04-06 09:08:42 -0300

    Segundo Commit

--- app/src/main/java/com/example/app23/MainActivity.kt
+++ app/src/main/java/com/example/app23/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.app23
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+// App23: Git, GitHub, GitHub Desktop
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val i = 10
     }
 }
